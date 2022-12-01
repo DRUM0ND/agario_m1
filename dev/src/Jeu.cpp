bde18af15d342838df5fa31671eb4eecd7862a00
Baptiste <baptiste.cld@laposte.net>
2022-12-01 17:29:40 +0100

    initial commit and random ia displacement

---
+++ dev/src/Jeu.cpp
@@ -0,0 +1,107 @@
+#include <SDL.h>
+#include <iostream>
+#include <cstdlib>
+#include "Jeu.hpp"
+#include "ReglageJeu.hpp"
+
+Jeu::Jeu(): joueurPrincipal(ReglageJeu::LARGEUR_FENETRE/2.0, ReglageJeu::HAUTEUR_FENETRE/2.0, 30.0)
+{
+	srand(time(NULL));
+    SDL_Init(SDL_INIT_EVERYTHING);
+    win = SDL_CreateWindow("AgarIO CPP",
+                           SDL_WINDOWPOS_CENTERED,
+                           SDL_WINDOWPOS_CENTERED,
+                           ReglageJeu::LARGEUR_FENETRE, ReglageJeu::HAUTEUR_FENETRE, SDL_WINDOW_RESIZABLE);
+    renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_PRESENTVSYNC);
+    joueurPrincipal.r = 255; joueurPrincipal.g = 255;  joueurPrincipal.b = 255;
+    joueurPrincipal.corps.w = 20.0;
+    joueurPrincipal.corps.h = 20.0;
+    joueurPrincipal.vboost = 2;
+
+    joueurPrincipal.updateVitesse();
+    plateau.generationJoueurs(ReglageJeu::NB_PLAYER);
+}
+
+Jeu::~Jeu(){
+    SDL_DestroyWindow(win);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+}
+
+bool Jeu::tick()
+{
+    SDL_SetRenderDrawColor(renderer, 230, 230, 230, 255);
+    SDL_RenderClear(renderer);
+    SDL_Event e;
+    plateau.fenetreXorigin = joueurPrincipal.corps.x - (ReglageJeu::LARGEUR_FENETRE/2);
+    plateau.fenetreYorigin = joueurPrincipal.corps.y - (ReglageJeu::HAUTEUR_FENETRE/2);
+    for (int i = 0 ; i <=ReglageJeu::HAUTEUR_MONDE_JEU; i++)
+    {
+      if (i >= plateau.fenetreYorigin &&
+          i <= plateau.fenetreYorigin+ReglageJeu::HAUTEUR_FENETRE && i%50==0){
+          SDL_SetRenderDrawColor(renderer, 90, 90, 90, 255);
+          
+          SDL_RenderDrawLine(renderer, 0-plateau.fenetreXorigin, i-plateau.fenetreYorigin,
+                             ReglageJeu::LARGEUR_MONDE_JEU-plateau.fenetreXorigin, i-plateau.fenetreYorigin);
+      }
+    }
+    for (int i = 0 ; i <= ReglageJeu::LARGEUR_MONDE_JEU; i++)
+    {
+      if (i >= plateau.fenetreXorigin &&
+          i <= plateau.fenetreXorigin+ReglageJeu::LARGEUR_FENETRE && i%50==0){
+          SDL_SetRenderDrawColor(renderer, 90, 90, 90, 255);
+          SDL_RenderDrawLine(renderer, i-plateau.fenetreXorigin,-plateau.fenetreYorigin,
+                             i-plateau.fenetreXorigin, ReglageJeu::HAUTEUR_MONDE_JEU-plateau.fenetreYorigin);
+      }
+    }
+    while ( SDL_PollEvent(&e))
+    {
+        switch (e.type)
+        {
+            case SDL_MOUSEMOTION:
+            {
+                double px = joueurPrincipal.corps.x, py = joueurPrincipal.corps.y;
+                plateau.convertToFenetreCoords(px, py);
+                double dx = e.motion.x - px;
+                double dy = e.motion.y - py;
+                joueurPrincipal.changeDirectionVitesse(dx, dy);
+            }
+                break;
+            case SDL_QUIT:
+                return false;
+        }
+        
+    }
+    joueurPrincipal.deplace();
+    plateau.afficheJoueur(joueurPrincipal,renderer);
+    
+    for (auto &i: plateau.joueurs){
+        if (i.estVisible){
+            i.deplace();
+        }
+        int applyRandomisation = rand()%100 == 1;
+        if (applyRandomisation) {
+        	double newRandomX = rand()%100-50;
+        	double newRandomY = rand()%100-50;
+        	i.changeDirectionVitesse(newRandomX, newRandomY);
+		}
+        
+        if (i.collision(joueurPrincipal) && i.estVisible && joueurPrincipal.estVisible){
+            if (i.peutManger(joueurPrincipal)){
+                i.mange(joueurPrincipal);
+            }else{
+                joueurPrincipal.mange(i);
+            }
+        }
+        if(plateau.estJoueurDansFenetre(i)){
+            plateau.afficheJoueur(i,renderer);
+        }
+        if ((rand()%10000) == 0) {
+            plateau.ajoutStaticElem(1);
+        }
+        
+    }
+    
+    SDL_RenderPresent(renderer);
+    return joueurPrincipal.estVisible;
+}
---
+++ dev/src/Joueur.cpp
@@ -0,0 +1,101 @@
+
+#include <SDL.h>
+#include "Joueur.hpp"
+#include <iostream>
+Joueur::Joueur()
+{
+    
+}
+
+Joueur::Joueur(double x, double y, double taille): taille(taille)
+{
+    corps.x = x;
+    corps.y = y;
+    updateVitesse();
+}
+
+
+Joueur::~Joueur(){
+}
+
+void
+Joueur::changeDirectionVitesse(double nx, double ny){
+    double normeN = sqrt(nx*nx+ny*ny);
+    double nxx = nx/normeN;
+    double nyy = ny/normeN;
+    double norme = sqrt(vx*vx+vy*vy);
+    vx = nxx*norme;
+    vy = nyy*norme;
+}
+
+
+// Regulation de la norme de la vitesse en fonction du la taille.
+void
+Joueur::updateVitesse()
+{
+    double norme = sqrt(vx*vx+vy*vy);
+    double regulNorme = 25.0/corps.w * vboost;
+    vx = (vx/norme)*regulNorme;
+    vy = (vy/norme)*regulNorme;
+}
+
+bool
+Joueur::collision(const Joueur &joueur)
+{
+    SDL_Rect cd = {(int)corps.x, (int)corps.y, (int)corps.w, (int)corps.h };
+    SDL_Rect cdj = {(int)joueur.corps.x, (int)joueur.corps.y, (int)joueur.corps.w, (int)joueur.corps.h };
+    return SDL_HasIntersection(&cd, &cdj);
+}
+
+void
+Joueur::mange(Joueur &joueur)
+{
+    joueur.estVisible = false;
+    corps.w += (joueur.corps.w/100.0)*30.0;
+    corps.h += (joueur.corps.h/100.0)*30.0;
+    vx -= vx/50.0;
+    vy -= vy/50.0;
+
+}
+
+void
+Joueur::deplace(){
+    corps.x += vx;
+    corps.y += vy; 
+    if (corps.x + corps.w > ReglageJeu::LARGEUR_MONDE_JEU)
+    {
+        corps.x = ReglageJeu::LARGEUR_MONDE_JEU - corps.w;
+    }
+    if (corps.y + corps.h > ReglageJeu::HAUTEUR_MONDE_JEU)
+    {
+        corps.y = ReglageJeu::HAUTEUR_MONDE_JEU -  corps.h;
+    }
+    if (corps.x < 0.0)
+    {
+        corps.x = 0.0;
+    }
+    if (corps.y < 0.0)
+    {
+        corps.y = 0.0;
+    }
+}
+
+void
+Joueur::incrementeVitesse(double varX, double varY){
+    vx += varX;
+    vy += varY;
+    if (abs(vx+varX) < vittMax)
+    {
+        vx += varX;
+    }
+    if (abs(vy+varY) < vittMax)
+    {
+        vy += varY;
+    }
+
+}
+
+bool
+Joueur::peutManger(const Joueur &joueur){
+    return corps.w > joueur.corps.w;
+}
